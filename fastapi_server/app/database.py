82217ad92f8001baf21d9e8a1383a364e123f4f8
DPChanny <kyoungchan0516@gmail.com>
2025-03-27 02:12:25 +0900

    fastapi db connection success healthy check condition reinforcement

--- fastapi_server/app/database.py
+++ fastapi_server/app/database.py
@@ -1,13 +1,36 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from config import DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USER
+from tenacity import retry, wait_fixed, stop_after_attempt, before_log
+import logging
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
 
 DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-engine = create_engine(
-    DATABASE_URL,
-    pool_timeout=30, #연결 시간 초과 제한
-    pool_pre_ping=True # DB 연결이 끊어졌는 지 주기적으로 확인
-)
 
-SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+@retry(
+    stop=stop_after_attempt(20),
+    wait=wait_fixed(5),
+    before=before_log(logger, logging.INFO)
+)
+def get_engine():
+    engine = create_engine(
+                DATABASE_URL,
+                pool_timeout=3,
+                pool_pre_ping=True,
+                future=True,
+                echo=True
+            )
+    with engine.connect() as conn:
+        logger.info("connect success")
+
+engine = None
+SessionLocal = None
 Base = declarative_base()
+
+def init_engine():
+    global engine, SessionLocal
+    engine = get_engine()
+    SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False, future=True)
+    
--- fastapi_server/app/main.py
+++ fastapi_server/app/main.py
@@ -1,17 +1,11 @@
 from fastapi import FastAPI
 from routers.test_router import router
-from database import engine
-from sqlalchemy import text
+import database
 
 app = FastAPI()
 
-app.include_router(router, prefix="/api")
-
-print("hi")
-
 @app.on_event("startup")
-def startup_event():
-    print("✅ DB 연결 성공 1 (startup에서)")
-    with engine.connect() as conn:
-        conn.execute(text("SELECT 1"))
-    print("✅ DB 연결 성공 2 (startup에서)")
+def on_startup():
+    database.init_engine()
+
+app.include_router(router, prefix="/api")
