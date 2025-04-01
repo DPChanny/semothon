d3915d8a98a48ed9bf535c94450230b1eab003ec
youngchae <seoyc0219@khu.ac.kr>
2025-04-02 02:15:24 +0900

    hh

--- flutter_app/lib/pages/user_input/register_complete_page.dart
+++ flutter_app/lib/pages/user_input/register_complete_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-
 import 'package:flutter_app/dto/user_register_dto.dart';
 
 class RegisterCompletePage extends StatelessWidget {
@@ -16,53 +15,29 @@ class RegisterCompletePage extends StatelessWidget {
           child: Padding(
             padding: const EdgeInsets.symmetric(horizontal: 24),
             child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start, // ✅ 텍스트 좌측 정렬
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                const SizedBox(height: 80), // ✅ 위로 올리기
+                const SizedBox(height: 80),
                 SvgPicture.asset(
-                  'assets/check_icon_line.svg', // ❗ 아이콘 경로 확인!
+                  'assets/check_icon_line.svg',
                   width: 78,
                   height: 78,
                 ),
-<<<<<<< HEAD
                 const SizedBox(height: 32),
                 Text(
-                  '${userInfo.name} 님\n정보 입력이 완료되었습니다.',
+                  '${UserRegisterDTO.instance.nickname} 님\n정보 입력이 완료되었습니다.',
                   style: const TextStyle(
                     color: Colors.black,
                     fontSize: 24,
                     fontWeight: FontWeight.w700,
                     height: 1.42,
                     letterSpacing: -0.41,
-=======
-              ),
-              const SizedBox(height: 32),
-              Text(
-                '${UserRegisterDTO.instance.nickname} 님\n정보 입력이 완료되었습니다.',
-                style: const TextStyle(
-                  color: Colors.black,
-                  fontSize: 24,
-                  fontWeight: FontWeight.w700,
-                  height: 1.42,
-                  letterSpacing: -0.41,
-                  fontFamily: 'Noto Sans KR',
-                ),
-              ),
-              const Spacer(),
-              Center(
-                child: const Text(
-                  '맞춤 서비스를 위해\n키워드 선택을 시작하겠습니다.',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 15,
->>>>>>> 2e4a7e56735902fd384859e73a807151489cee02
                     fontFamily: 'Noto Sans KR',
                   ),
                 ),
-<<<<<<< HEAD
                 const Spacer(),
-                Center(
-                  child: const Text(
+                const Center(
+                  child: Text(
                     '맞춤 서비스를 위해\n키워드 선택을 시작하겠습니다.',
                     textAlign: TextAlign.center,
                     style: TextStyle(
@@ -72,27 +47,6 @@ class RegisterCompletePage extends StatelessWidget {
                       fontWeight: FontWeight.w400,
                       height: 1.53,
                       letterSpacing: -0.26,
-=======
-              ),
-              const SizedBox(height: 20),
-              Center(
-                child: SizedBox(
-                  width: 335,
-                  height: 47,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      Navigator.pushNamedAndRemoveUntil(
-                        context,
-                        '/home_page',
-                        (Route<dynamic> route) => false,
-                      );
-                    },
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: const Color(0xFF008CFF),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(23.5),
-                      ),
->>>>>>> 2e4a7e56735902fd384859e73a807151489cee02
                     ),
                   ),
                 ),
@@ -103,7 +57,11 @@ class RegisterCompletePage extends StatelessWidget {
                     height: 47,
                     child: ElevatedButton(
                       onPressed: () {
-                        Navigator.popUntil(context, (route) => route.isFirst);
+                        Navigator.pushNamedAndRemoveUntil(
+                          context,
+                          '/home_page',
+                              (Route<dynamic> route) => false,
+                        );
                       },
                       style: ElevatedButton.styleFrom(
                         backgroundColor: const Color(0xFF008CFF),
