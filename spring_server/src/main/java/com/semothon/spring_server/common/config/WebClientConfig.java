a951bcc30ff4cb33ba20e803246313bd3f7f4b1e
DPChanny <kyoungchan0516@gmail.com>
2025-04-03 15:09:31 +0900

    hot fix

--- spring_server/src/main/java/com/semothon/spring_server/common/config/WebClientConfig.java
+++ spring_server/src/main/java/com/semothon/spring_server/common/config/WebClientConfig.java
@@ -4,12 +4,15 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Configuration
 public class WebClientConfig {
 
     @Bean
     public WebClient webClient(@Value("${external.fastapi.url}") String fastApiBaseUrl) {
+        log.info("WebClient baseUrl = {}", fastApiBaseUrl);
         return WebClient.builder()
                 .baseUrl(fastApiBaseUrl)
                 .build();
