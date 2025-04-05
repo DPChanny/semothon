1f1337d2c4d73a0d8dd47d5b29dba17008e8c46d
Namwoosung <142873609+Namwoosung@users.noreply.github.com>
2025-04-05 12:37:54 +0900

    add chatRoom api

--- spring_server/src/main/java/com/semothon/spring_server/chat/controller/ChatController.java
+++ spring_server/src/main/java/com/semothon/spring_server/chat/controller/ChatController.java
@@ -1,15 +1,24 @@
 package com.semothon.spring_server.chat.controller;
 
 import com.semothon.spring_server.chat.dto.ChatMessageRequestDto;
+import com.semothon.spring_server.chat.dto.ChatMessageResponseDto;
+import com.semothon.spring_server.chat.dto.GetChatRoomResponseDto;
+import com.semothon.spring_server.chat.service.ChatRoomService;
 import com.semothon.spring_server.chat.service.ChatService;
+import com.semothon.spring_server.common.dto.BaseResponse;
 import com.semothon.spring_server.common.exception.ForbiddenException;
+import com.semothon.spring_server.user.entity.User;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @RestController
@@ -17,6 +26,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class ChatController {
 
     private final ChatService chatService;
+    private final ChatRoomService chatRoomService;
 
     // 클라이언트가 /pub/chat/message 로 보낸 메시지 처리
     @MessageMapping("/chat/message")
@@ -30,5 +40,25 @@ public class ChatController {
         log.info("메시지 수신: userId={}, chatRoomId={}, message={}", userId, message.getChatRoomId(), message.getMessage());
 
         chatService.handleChatMessage(message, userId);
+    }
+
+    @GetMapping("/api/chats/{chatRoomId}")
+    @ResponseStatus(HttpStatus.OK)
+    public BaseResponse getChatRoomInfo(
+            @PathVariable Long chatRoomId
+    ){
+        GetChatRoomResponseDto chatRoomResponseDto = chatRoomService.getChatRoom(chatRoomId);
+        return BaseResponse.success(Map.of("code", 200, "chatRoom", chatRoomResponseDto), "ChatRoom retrieved successfully");
+    }
+
+    @GetMapping("/api/chats/{chatRoomId}/messages")
+    @ResponseStatus(HttpStatus.OK)
+    public BaseResponse getMessages(
+            @AuthenticationPrincipal User user,
+            @PathVariable Long chatRoomId
+    ){
+        String currentUserId = user.getUserId();
+        List<ChatMessageResponseDto> messages = chatRoomService.getMessages(chatRoomId, currentUserId);
+        return BaseResponse.success(Map.of("code", 200, "chatMessages", messages), "ChatMessages retrieved successfully");
     }
 }
--- spring_server/src/main/java/com/semothon/spring_server/chat/repository/ChatMessageRepository.java
+++ spring_server/src/main/java/com/semothon/spring_server/chat/repository/ChatMessageRepository.java
@@ -4,6 +4,10 @@ import com.semothon.spring_server.chat.entity.ChatMessage;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
+    List<ChatMessage> findAllByChatRoom_ChatRoomIdOrderByCreatedAtAsc(Long chatRoomId);
+
 }
