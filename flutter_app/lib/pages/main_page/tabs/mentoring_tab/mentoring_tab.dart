d1996db398f18e1a5dfc69e94f98ea455d06b25d
DPChanny <kyoungchan0516@gmail.com>
2025-04-06 05:08:17 +0900

    button and search detail complete

--- flutter_app/lib/pages/main_page/tabs/mentoring_tab/mentoring_tab.dart
+++ flutter_app/lib/pages/main_page/tabs/mentoring_tab/mentoring_tab.dart
@@ -3,6 +3,7 @@ import 'package:flutter_app/pages/main_page/tabs/mentoring_tab/tabs/my_mentor_ta
 import 'package:flutter_app/pages/main_page/tabs/mentoring_tab/tabs/recommended_mentor_tab.dart';
 import 'package:flutter_app/pages/main_page/tabs/mentoring_tab/tabs/recommended_room_tab.dart';
 import 'package:flutter_app/pages/main_page/tabs/mentoring_tab/tabs/search_tab.dart';
+import 'package:flutter_app/routes/mentoring_tab_routes.dart';
 import 'package:flutter_app/widgets/custom_tab_bar.dart';
 
 class MentoringTab extends StatefulWidget {
@@ -29,34 +30,6 @@ class _MentoringTabState extends State<MentoringTab> {
       backgroundColor: Colors.white,
       body: Column(
         children: [
-          GestureDetector(
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => SearchPage()),
-              );
-            },
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Container(
-                decoration: BoxDecoration(
-                  color: const Color(0xFFE4E4E4),
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 14),
-                child: Row(
-                  children: [
-                    const Icon(Icons.search, color: Colors.grey),
-                    const SizedBox(width: 10),
-                    const Text(
-                      '멘토 검색',
-                      style: TextStyle(color: Colors.grey),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ),
           CustomTabBar(
             labels: tabLabels,
             selectedIndex: _selectedTabIndex,
