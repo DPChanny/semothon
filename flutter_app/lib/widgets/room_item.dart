56c8ca46d8de17bf7f2df2bbac2d7876e2222ffa
DPChanny <kyoungchan0516@gmail.com>
2025-04-06 00:58:11 +0900

    search_tab.dart complete

--- flutter_app/lib/widgets/room_item.dart
+++ flutter_app/lib/widgets/room_item.dart
@@ -12,17 +12,6 @@ class RoomItem extends StatelessWidget {
     return Container(
       margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
       padding: const EdgeInsets.all(12),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.zero,
-        boxShadow: const [
-          BoxShadow(
-            color: Color.fromARGB(31, 8, 0, 0),
-            blurRadius: 4,
-            offset: Offset(0, 2),
-          ),
-        ],
-      ),
       child: Row(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
