5825add385f07709e3be997386fb3a975cf946fc
DPChanny <kyoungchan0516@gmail.com>
2025-04-27 03:20:45 +0900

    fastapi_server submoduled

--- fastapi_server/app/ai/descripable.py
+++
@@ -1,7 +0,0 @@
-class Descriptable:
-    description: str
-    id: str
-
-    def __init__(self, description, id):
-        self.description = description
-        self.id = id
--- fastapi_server/app/dto/intro_dto.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-
-class IntroRequestDTO(BaseModel):
-    user_id: str
-
-class IntroResponseDTO(BaseModel):
-    success: bool
-    intro: str
-    message: str
